7efe31e379ef3bdc136c6c946fc8e9115a096dfb
berkeoral <berke.oral@gmail.com>
2016-10-30 23:32:25 +0200

    Added initDataBase funtion to server.py

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
 import os
 import psycopg2 as dbapi2
+import json
+import re
 
 from flask import Flask
+from flask import redirect
+from flask.helpers import url_for
 from flask import render_template
 
 
@@ -32,10 +36,42 @@ def places_page():
 def photos_page():
     return render_template('photos.html')
 
+@app.route('/initdb')
+def initDataBase():
+    with dbapi.connect(app.config['dsn']) as connection:
+        cursor = connection.cursor()
+        ###
+        query = """DROP TABLE IF EXISTS EVENT_COUNTER"""
+        cursor.execute(query)
+        query = """DROP TABLE IF EXISTS EVENT"""
+        cursor.execute(query)
+        query = """CREATE TABLE EVENT_COUNTER (N INTEGER)"""
+        cursor.execute(query)
+        query = """CREATE TABLE EVENT_COUNTER (N INTEGER)"""
+        cursor.execute(query)
+        query = """INSERT INTO EVENT_COUNTER VALUES (0)"""
+        cursor.execute(query)
+        query = """CREATE TABLE EVENT (
+                NAME VARCHAR(50) NOT NULL,
+                DESCRIPTION VARCHAR(50) NOT NULL,
+                STARTING_DATE DATETIME,
+                ENDING_DATE DATETIME,
+                PLACE VARCHAR(100),
+                )"""
+        cursor.execute(query)
+        ###
+        connection.commit()
+
 if __name__ == '__main__':
     VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
     if VCAP_APP_PORT is not None:
         port, debug = int(VCAP_APP_PORT), False
     else:
         port, debug = 5000, True
+        VCAP_SERVICES = os.getenv('VCAP_SERVICES')
+    if VCAP_SERVICES is not None:
+        app.config['dsn'] = get_elephantsql_dsn(VCAP_SERVICES)
+    else:
+        app.config['dsn'] = """user='vagrant' password='vagrant'
+                               host='localhost' port=54321 dbname='itucsdb'"""
     app.run(host='0.0.0.0', port=port, debug=debug)
